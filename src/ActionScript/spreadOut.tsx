4c4cb6bc2489eeeb3948e9f45205293b4d792249
milky <listingslab@gmail.com>
2022-04-02 20:35:20 +1000

    Spread out

--- src/ActionScript/spreadOut.tsx
+++ src/ActionScript/spreadOut.tsx
@@ -1,17 +1,44 @@
 import { getElement, animateTo, setPosition } from "./";
 
+const layoutMobile = (clips) => {
+  let totalHeight = 0;
+  for (let i = 0; i < clips.length; i++) {
+    const { id } = clips[i];
+    // @ts-ignore
+    totalHeight = totalHeight + getElement(id).height;
+  }
+  for (let j = 0; j < clips.length; j++) {
+    const { id } = clips[j];
+    const adjustedTop = 10 + j * 125;
+    setPosition(id, "topmiddle", { left: 0, top: adjustedTop });
+  }
+  return true;
+};
+
+const layoutDesktop = (clips) => {
+  // console.warn("layoutDesktop");
+  setPosition(clips[0].id, "topmiddle", { left: -175, top: 32 });
+  setPosition(clips[1].id, "topmiddle", { left: 350, top: 32 });
+  return true;
+};
+
 export const spreadOut = (clips) => {
   try {
-    let totalHeight = 0;
-    for (let i = 0; i < clips.length; i++) {
-      const { id } = clips[i];
-      // @ts-ignore
-      totalHeight = totalHeight + getElement(id).height;
+    let layout = "mobile";
+    const stage = getElement("flashStage");
+    // @ts-ignore
+    const { width, mode } = stage;
+    if (width > 690 && mode === "landscape") {
+      // console.warn("do this");
+      layout = "desktop";
     }
-    for (let j = 0; j < clips.length; j++) {
-      const { id } = clips[j];
-      const adjustedTop = 10 + j * 125;
-      setPosition(id, "topmiddle", { left: 0, top: adjustedTop });
+
+    switch (layout) {
+      case "desktop":
+        layoutDesktop(clips);
+        break;
+      default:
+        layoutMobile(clips);
     }
   } catch (error) {
     return false;
